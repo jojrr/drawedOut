2dbc8eb2dd578a943d63d599fcc7613a632e6e36
jojrr <rulergeorge@gmail.com>
2025-11-24 18:31:43 +0000

    removed MeleeEnemy.cs now in Enemy.cs file

--- drawedOut/Enemy.cs
+++ drawedOut/Enemy.cs
@@ -33,5 +33,14 @@ namespace drawedOut
             InactiveEnemyList.Remove(this);
         }
     }
+
+
+    internal class MeleeEnemy : Enemy
+    {
+        public MeleeEnemy(Point origin, int width, int height, int hp, int xAccel)
+            : base(origin: origin, width: width, height: height, hp: hp, xAccel: xAccel)
+        {
+        }
+    }
 }
 
